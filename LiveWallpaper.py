00e2eb20328c64e90a21f74f59437388a4d1f583
Xyonox <93388182+Xyonox@users.noreply.github.com>
2024-07-29 18:14:33 +0200

    setup, set_wallpaper

---
+++ LiveWallpaper.py
@@ -0,0 +1,30 @@
+import sys
+import platform
+
+def set_wallpaper(image_path):
+    system = platform.system()
+
+    if system == "Windows":
+        import ctypes
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
+    elif system == "Darwin":  # macOS
+        import subprocess
+        script = f'''
+        tell application "System Events"
+            set picture of current desktop to POSIX file "{image_path}"
+        end tell
+        '''
+        subprocess.run(["osascript", "-e", script])
+    elif system == "Linux":
+        import subprocess
+        subprocess.run(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", f"file://{image_path}"])
+    else:
+        raise NotImplementedError(f"Das Ändern des Hintergrundbilds ist für {system} nicht implementiert.")
+
+
+# das
+def animate_wallpaper(frames):
+    print()
+
+def video_to_frames(vid):
+    print()
